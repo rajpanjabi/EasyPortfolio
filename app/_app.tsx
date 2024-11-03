5a6912e703033d09b0458024e70ff9bf1e41a355
Raj <rajpunjabi47@yahoo.in>
2024-11-03 14:03:00 -0500

    basic skeleton and setup

---
+++ app/_app.tsx
@@ -0,0 +1,14 @@
+import '@/app/styles/globals.scss';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '@/app/redux/store';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+
+
+// app/about/page.tsx
+export default function About() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>This is the About page content.</p>
+    </main>
+  );
+}
